313ff56b16cd3f535a9c8c7dcb08f34b162a5eae
Charles Smith <smithc14@macewan.ca>
2019-02-28 20:00:01 -0700

    Fixing header handling (back to specifying them as a map of strings).

--- config.go
+++ config.go
@@ -1,53 +1,53 @@
 package http
 
 import (
-  "time"
+	"time"
 
-  "github.com/elastic/beats/libbeat/outputs"
+	"github.com/elastic/beats/libbeat/outputs"
 )
 
 type httpConfig struct {
-  Protocol         string             `config:"protocol"`
-  Path             string             `config:"path"`
-  Params           map[string]string  `config:"parameters"`
-  Username         string             `config:"username"`
-  Password         string             `config:"password"`
-  ProxyURL         string             `config:"proxy_url"`
-  LoadBalance      bool               `config:"loadbalance"`
-  BatchPublish     bool               `config:"batch_publish"`
-  BatchSize        int                `config:"batch_size"`
-  CompressionLevel int                `config:"compression_level" validate:"min=0, max=9"`
-  TLS              *outputs.TLSConfig `config:"tls"`
-  MaxRetries       int                `config:"max_retries"`
-  Timeout          time.Duration      `config:"timeout"`
-  Headers          string             `config:"headers"`
-  ContentType      string             `config:"content_type"`
+	Protocol         string             `config:"protocol"`
+	Path             string             `config:"path"`
+	Params           map[string]string  `config:"parameters"`
+	Username         string             `config:"username"`
+	Password         string             `config:"password"`
+	ProxyURL         string             `config:"proxy_url"`
+	LoadBalance      bool               `config:"loadbalance"`
+	BatchPublish     bool               `config:"batch_publish"`
+	BatchSize        int                `config:"batch_size"`
+	CompressionLevel int                `config:"compression_level" validate:"min=0, max=9"`
+	TLS              *outputs.TLSConfig `config:"tls"`
+	MaxRetries       int                `config:"max_retries"`
+	Timeout          time.Duration      `config:"timeout"`
+	Headers          map[string]string  `config:"headers"`
+	ContentType      string             `config:"content_type"`
 }
 
 var (
-  defaultConfig = httpConfig{
-    Protocol:         "",
-    Path:             "",
-    ProxyURL:         "",
-    Username:         "",
-    Password:         "",
-    BatchPublish:     false,
-    BatchSize:        100,
-    Timeout:          90 * time.Second,
-    CompressionLevel: 0,
-    TLS:              nil,
-    MaxRetries:       3,
-    LoadBalance:      true,
-    ContentType:      "application/json; charset=UTF-8",
-  }
+	defaultConfig = httpConfig{
+		Protocol:         "",
+		Path:             "",
+		ProxyURL:         "",
+		Username:         "",
+		Password:         "",
+		BatchPublish:     false,
+		BatchSize:        100,
+		Timeout:          90 * time.Second,
+		CompressionLevel: 0,
+		TLS:              nil,
+		MaxRetries:       3,
+		LoadBalance:      true,
+		ContentType:      "application/json; charset=UTF-8",
+	}
 )
 
 func (c *httpConfig) Validate() error {
-  if c.ProxyURL != "" {
-    if _, err := parseProxyURL(c.ProxyURL); err != nil {
-      return err
-    }
-  }
+	if c.ProxyURL != "" {
+		if _, err := parseProxyURL(c.ProxyURL); err != nil {
+			return err
+		}
+	}
 
-  return nil
+	return nil
 }
--- http.go
+++ http.go
@@ -1,161 +1,146 @@
 package http
 
 import (
-  "errors"
-  "net/url"
-  "strings"
-  "time"
-  "fmt"
-  "strconv"
+	"errors"
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
 
-  "github.com/elastic/beats/libbeat/beat"
-  "github.com/elastic/beats/libbeat/common"
-  "github.com/elastic/beats/libbeat/logp"
-  "github.com/elastic/beats/libbeat/outputs"
+	"github.com/elastic/beats/libbeat/beat"
+	"github.com/elastic/beats/libbeat/common"
+	"github.com/elastic/beats/libbeat/logp"
+	"github.com/elastic/beats/libbeat/outputs"
 )
 
 func init() {
-  // outputs.RegisterOutputPlugin("http", New)
-  outputs.RegisterType("http", makeHTTP)
+	// outputs.RegisterOutputPlugin("http", New)
+	outputs.RegisterType("http", makeHTTP)
 }
 
 var (
-  debugf = logp.MakeDebug("http")
+	debugf = logp.MakeDebug("http")
 )
 
 var (
-  // ErrNotConnected indicates failure due to client having no valid connection
-  ErrNotConnected = errors.New("not connected")
+	// ErrNotConnected indicates failure due to client having no valid connection
+	ErrNotConnected = errors.New("not connected")
 
-  // ErrJSONEncodeFailed indicates encoding failures
-  ErrJSONEncodeFailed = errors.New("json encode failed")
+	// ErrJSONEncodeFailed indicates encoding failures
+	ErrJSONEncodeFailed = errors.New("json encode failed")
 )
 
 func makeHTTP(
-  beat beat.Info,
-  observer outputs.Observer,
-  cfg *common.Config,
+	beat beat.Info,
+	observer outputs.Observer,
+	cfg *common.Config,
 ) (outputs.Group, error) {
-  config := defaultConfig
-  logp.Info("Initializing HTTP output")
+	config := defaultConfig
+	logp.Info("Initializing HTTP output")
 
-  if err := cfg.Unpack(&config); err != nil {
-    return outputs.Fail(err)
-  }
+	if err := cfg.Unpack(&config); err != nil {
+		return outputs.Fail(err)
+	}
 
-  tlsConfig, err := outputs.LoadTLSConfig(config.TLS)
-  if err != nil {
-    return outputs.Fail(err)
-  }
+	tlsConfig, err := outputs.LoadTLSConfig(config.TLS)
+	if err != nil {
+		return outputs.Fail(err)
+	}
 
-  hosts, err := outputs.ReadHostList(cfg)
-  if err != nil {
-    return outputs.Fail(err)
-  }
+	hosts, err := outputs.ReadHostList(cfg)
+	if err != nil {
+		return outputs.Fail(err)
+	}
 
-  proxyURL, err := parseProxyURL(config.ProxyURL)
-  if err != nil {
-    return outputs.Fail(err)
-  }
-  if proxyURL != nil {
-    logp.Info("Using proxy URL: %s", proxyURL)
-  }
+	proxyURL, err := parseProxyURL(config.ProxyURL)
+	if err != nil {
+		return outputs.Fail(err)
+	}
+	if proxyURL != nil {
+		logp.Info("Using proxy URL: %s", proxyURL)
+	}
 
-  params := config.Params
-  if len(params) == 0 {
-    params = nil
-  }
+	params := config.Params
+	if len(params) == 0 {
+		params = nil
+	}
 
-  headers := make(map[string]string)
-  fields := strings.Split(config.Headers, "|")
-  if len(fields) % 2 == 0 {
-    for index := 0; index < len(fields); index+=2  {
-      logp.Debug("header field: %s %s", fields[index], fields[index+1])
-      headers[fields[index]] = fields[index+1]
-    }
-  } else {
-    logp.Info("No header fields found")
-  }
+	clients := make([]outputs.NetworkClient, len(hosts))
 
-  clients := make([]outputs.NetworkClient, len(hosts))
+	for i, host := range hosts {
+		logp.Info("Making client for host: " + host)
+		var name string
+		var port int
+		if config.Protocol == "https" {
+			port = 443
+		} else if config.Protocol == "http" {
+			port = 80
+		} else {
+			logp.Warn("Unknown protocol: %s", config.Protocol)
+		}
+		hostparts := strings.Split(host, ":")
 
-  for i, host := range hosts {
-    logp.Info("Making client for host: " + host)
-    var name string
-    var port int
-    if config.Protocol == "https" {
-      port = 443
-    } else if config.Protocol == "http"{
-      port = 80
-    } else {
-      logp.Warn("Unknown protocol: %s", config.Protocol)
-    }
-    hostparts := strings.Split(host, ":")
+		if len(hostparts) == 1 {
+			name = hostparts[0]
+		} else if len(hostparts) == 2 {
+			name = hostparts[0]
+			port, err = strconv.Atoi(hostparts[1])
+			if err != nil {
+				logp.Err("Invalid port number: %v, Error: %v", hostparts[1], err)
+				return outputs.Fail(err)
+			}
+		} else {
+			logp.Err("Unable to parse host:port from: %v", host)
+		}
 
-    if len(hostparts) == 1 {
-      name = hostparts[0]
-      for index := 0; index < len(fields); index+=2  {
-        logp.Debug("header field: %s %s", fields[index], fields[index+1])
-        headers[fields[index]] = fields[index+1]
-      }
-    } else if len(hostparts) == 2 {
-      name = hostparts[0]
-      port, err = strconv.Atoi(hostparts[1])
-      if err != nil {
-        logp.Err("Invalid port number: %v, Error: %v", hostparts[1], err)
-        return outputs.Fail(err)
-      }
-    } else {
-      logp.Err("Unable to parse host:port from: %v", host)
-    }
+		//hostURL, err := getURL(config.Protocol, port, config.Path, host)
+		hostURL := fmt.Sprintf("%v://%v:%v/%v", config.Protocol, name, port, config.Path)
+		if err != nil {
+			logp.Err("Invalid host param set: %s, Error: %v", name, err)
+			return outputs.Fail(err)
+		}
 
-    //hostURL, err := getURL(config.Protocol, port, config.Path, host)
-    hostURL := fmt.Sprintf("%v://%v:%v/%v", config.Protocol, name, port, config.Path)
-    if err != nil {
-      logp.Err("Invalid host param set: %s, Error: %v", name, err)
-      return outputs.Fail(err)
-    }
+		logp.Info("Final host URL: " + hostURL)
 
-    logp.Info("Final host URL: " + hostURL)
+		var client outputs.NetworkClient
+		client, err = NewClient(ClientSettings{
+			URL:              hostURL,
+			Proxy:            proxyURL,
+			TLS:              tlsConfig,
+			Username:         config.Username,
+			Password:         config.Password,
+			Parameters:       params,
+			Timeout:          config.Timeout,
+			CompressionLevel: config.CompressionLevel,
+			BatchPublish:     config.BatchPublish,
+			headers:          config.Headers,
+			ContentType:      config.ContentType,
+		})
 
-    var client outputs.NetworkClient
-    client, err = NewClient(ClientSettings{
-      URL:              hostURL,
-      Proxy:            proxyURL,
-      TLS:              tlsConfig,
-      Username:         config.Username,
-      Password:         config.Password,
-      Parameters:       params,
-      Timeout:          config.Timeout,
-      CompressionLevel: config.CompressionLevel,
-      BatchPublish:     config.BatchPublish,
-      headers:          headers,
-      ContentType:      config.ContentType,
-    })
+		if err != nil {
+			return outputs.Fail(err)
+		}
 
-    if err != nil {
-      return outputs.Fail(err)
-    }
+		// TODO: Backoff to be implemented
+		client = outputs.WithBackoff(client, 1*time.Second, 60*time.Second)
+		clients[i] = client
+	}
 
-    // TODO: Backoff to be implemented
-    client = outputs.WithBackoff(client, 1*time.Second, 60*time.Second)
-    clients[i] = client
-  }
-
-  return outputs.SuccessNet(config.LoadBalance, config.BatchSize, config.MaxRetries, clients)
+	return outputs.SuccessNet(config.LoadBalance, config.BatchSize, config.MaxRetries, clients)
 }
 
 func parseProxyURL(raw string) (*url.URL, error) {
-  if raw == "" {
-    return nil, nil
-  }
+	if raw == "" {
+		return nil, nil
+	}
 
-  url, err := url.Parse(raw)
-  if err == nil && strings.HasPrefix(url.Scheme, "http") {
-    return url, err
-  }
+	url, err := url.Parse(raw)
+	if err == nil && strings.HasPrefix(url.Scheme, "http") {
+		return url, err
+	}
 
-  // Proxy was bogus. Try prepending "http://" to it and
-  // see if that parses correctly.
-  return url.Parse("http://" + raw)
+	// Proxy was bogus. Try prepending "http://" to it and
+	// see if that parses correctly.
+	return url.Parse("http://" + raw)
 }
